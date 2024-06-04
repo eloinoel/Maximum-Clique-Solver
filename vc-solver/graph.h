03f43d17393aa46e9e50ab5df6e1ab87dc5b2581
eloinoel <eloi.sandt.games@gmx.de>
2024-06-04 15:12:30 +0200

    Add comments to graph class

--- vc-solver/graph.h
+++ vc-solver/graph.h
@@ -14,12 +14,17 @@
 
 using namespace std;
 
-
-
-
-enum states {UNKNOWN = -1, EXCLUDED, INCLUDED, FOLDED};
+/**
+ * UKNOWN: don't know whether is in VC, can be in graph or not
+ * EXCLUDED: can't be in VC, not in graph
+ * INCLUDED: is in VC, not in graph
+ * FOLDED: merged with other vertex, not in graph, not relevant for MC
+*/
+enum state {UNKNOWN = -1, EXCLUDED, INCLUDED, FOLDED};
 
 #define DEBUG 0
+
+/** not relevant for MC */
 constexpr bool UPDATE_G_EDGELIST = false;
 
 class Vertex;
@@ -27,7 +32,7 @@ class Edge;
 class Graph;
 class Operation;
 
-
+/** Endpoints of Edges */
 typedef struct{
         Vertex* v = nullptr;
         size_t idx = 0;
@@ -36,82 +41,113 @@ typedef struct{
 class Graph {
 public:
     size_t total_N = 0;   //total N, M of initial instance
-    size_t total_M = 0; 
+    size_t total_M = 0;
 
     size_t N = 0;   //remaining N, M
     size_t M = 0;
 
     //Counter c;// mostly debug helper, could be extended to collect profiling information
     Timer timer;
+    /** count num branches in search tree */
     unsigned long long num_branches = 0;
+    /** used to mark vertices, eg. to find common neighbours */
     unsigned long long timestamp = 0;
 
-
+    /* remaining budget for VC (used when searching for VC of size 1, 2, ...) */
     int k = 0;
+    /* current solution size */
     int sol_size = 0;
 
+    /* max degree in current graph */
     size_t max_degree = 0;
 
+    /* vertices are never really deleted, only status update*/
     vector<Vertex*> V;
     vector<Edge*> E;
 
+    /* deg_list[0] = vertices with deg(v)=0, use @max_degree to get max_deg bucket
+    * changes in graph change this list --> don't use for-loop to iterate
+    * iterate with: while(!empty) { take front/back }
+    */
     vector<vector<Vertex*>> deg_lists;
 
+    /**
+     * used to easily undo graph changes
+    */
     vector<Operation*> history;
 
+    /* map vertex index to string name */
     vector<string> name_table;
 
     // simple version at the moment, will need to be adjusting for vertex folding etc.
     void output_vc();
 
+    /* used for marking vertices */
     void new_timestamp();
     void increase_timestamp(unsigned long long offset);
 
-    void undo();
-    void restore();
+    /* set restore point in operation stack, eg. to restore to this point after branching*/
     void set_restore();
+    /* undo all operations until restore point created with `set_restore` */
+    void restore();
+    /* undo last operation on stack */
+    void undo();
 
-    Vertex* add_vertex();
-    Vertex* add_vertex(size_t id);
 
+    /* add vertex at the end */
+    Vertex* add_vertex(); // used to construct graph
+    Vertex* add_vertex(size_t id); // only Bruno uses this
+
+    /* INTERNAL METHOD, only for Bruno
+    */
     void restore_vertex(Vertex* v);
 
-    // use when vertex state stays UNKNOWN
-    void delete_vertex(Vertex* v); 
+    /* INTERNAL METHOD, only for Bruno
+    * use when vertex state stays UNKNOWN
+    */
+    void delete_vertex(Vertex* v);
 
-    /** 
+    /**---------------------------------------------------------------------
      * Functions prefixed with MM_ are already memory managed
      * that is, the changes of the operations are saved in the history
      * and can be automatically reversed with undo()
      */
 
+    /* use to EXCLUDE vertex from solution */
+    void MM_discard_vertex(Vertex* v);
+    /* use to INCLUDE vertex to solution */
+    void MM_select_vertex(Vertex* v);
+    /* EXCLUDE vertex from solution and INCLUDE N(v)*/
+    void MM_select_neighbors(Vertex* v);
     /**
      * all of these assume that the vertex is not deg0 and do not check this
      * this includes the case you call discard twice, a neighbor may become deg0 and error
      * could change this/add a _SAFE variant when explicitely deleting multiple times without other checks in between
      */
-    // use when vertex state is EXCLUDED
-    void MM_discard_vertex(Vertex* v);
-    // use when vertex state is INCLUDED
-    void MM_select_vertex(Vertex* v);
-    // use when vertex state is EXCLUDED and N(v) INCLUDED
-    void MM_select_neighbors(Vertex* v);
+    // ---------------------------------------------------------------------
 
+    /*  */
     Edge* add_edge(Vertex* u, Vertex* v);
 
-    // does not track any information for restore
+    /*
+    * does not track any information for restore
+    */
     void delete_edge(Edge* e);
-    /**
+    /** INTERNAL, is used by `delete_edge`
      * keeps information for easy restore
      * use for simple case of deleting a vertex that will be restored to exact same state
      */
     void delete_edge_lazy(Edge* e, Vertex* v);
 
-
+    /* INTERNAL */
     void update_deglists(Vertex* v);
-    // for moving between deglists
+    /* INTERNAL
+     * for moving between deglists
+     */
     void remove_from_deglist(Vertex* v);
-    // when Vertex won't be in any list afterwards
+    /* INTERNAL
+     * when Vertex won't be in any list afterwards
+     */
     void delete_from_deglist(Vertex* v);
 
 };
@@ -124,17 +160,17 @@ public:
     #endif
 
     size_t id;
-    int8_t status = UNKNOWN; // -1 = unknown, 0 = deleted, 1 = solution, 2 = folded
+    state status = UNKNOWN;
     unsigned long long marked = 0;
 
     size_t deg_idx = -1; // index in degree list, needed to remove without search
     size_t list_idx = 0;
-    bool deleted = false;
+    bool deleted = false; // check status instead, Bruno is not sure if this will be needed
 
-    std::vector<Vertex*> neighbors; 
+    std::vector<Vertex*> neighbors;
     std::vector<Edge*> edges;
 
-    union data_union{
+    union data_union {
         struct{
             size_t clique_idx = 0;
             //size_t clique_size = 0;
@@ -142,9 +178,9 @@ public:
 
         data_union(){};
         ~data_union(){};
-    }data;
+    } data;
 
-    /** 
+    /**
      * linear search through smaller neighborhood, sufficient for sparse graphs
      * however, since clique will usually deal with dense ones, maybe change?
      * potentially tree for log complexity?
@@ -161,8 +197,7 @@ class Edge{
 public:
     Endpoint ends[2];
 
-    size_t idx;
-    
+    size_t idx; // Edge itself has an index
 };
 
 
