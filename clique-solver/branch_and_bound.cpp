ead47ee075be837743fee25cad07ccca0d7c84cc
Elias Inguanta <inguanta@campus.tu-berlin.de>
2024-06-15 13:24:40 +0200

    Simplification of the BnB algorithm and use of MC-Graph functions

--- clique-solver/branch_and_bound.cpp
+++ clique-solver/branch_and_bound.cpp
@@ -1,56 +1,21 @@
 #include "../vc-solver/graph.h"
 
 /**
- * @brief Get one candidate v ∈ P
+ * @brief Get candidate set P
  * 
  * @param G out current Graph
- * @return candidate v ∈ P
+ * @return candidate set P
  */
-Vertex* get_candidate(Graph& G){
+vector<Vertex*> get_candidates(Graph& G){
     
-    //we return the first vertex we find in the degree list
-    for(int i=1; i<=G.max_degree; i++){
-        for(Vertex* u: G.deg_lists[i]){
-            return u;
-        }
-    }
-
-    return nullptr;
-}
-
-/**
- * @brief We reduce P to P ∩ (N(v) ∪ {v})
- *
- * @param G our current graph
- * @param v our current branching vertex
- */
-void MM_discard_strangers(Graph& G, Vertex* v){
-
     std::vector<Vertex*> candidates;
-    std::vector<Vertex*> strangers;
-
-    //get the candidate set P stored implicitly in G
-    for(int i=G.max_degree; i>=1; i--){
-        for(Vertex* u: G.deg_lists[i]){
+    for(vector<Vertex*> list: G.deg_lists){
+        for(Vertex* u: list){
             candidates.push_back(u);
         }
     }
 
-    //if there is a candidate in P that is not in N(v) and not v itself then its a stranger
-    for(Vertex* candidate : candidates){
-        if(candidate!=v && !v->adjacent_to(candidate)){
-            strangers.push_back(candidate);
-        }
-    }
-
-    //EXCLUDE strangers
-    for(Vertex* stranger: strangers){
-        if(stranger->degree()>0){
-            G.MM_discard_vertex(stranger);
-        }
-    }
-
-
+    return candidates;
 }
 
 /**
@@ -67,24 +32,20 @@ void branch_and_bound(Graph& G, vector<Vertex*>& maximum_clique){ //BnB(P, C, C^
     G.set_restore();
 
     //if C > C^* then
-    if(G.sol_size+1>maximum_clique.size()){
-
+    if(G.partial.size()>maximum_clique.size()){
         //C^* = C
-        maximum_clique.clear();
-        for(Vertex* v : G.V){
-            if(v->status == CLIQUE){
-                maximum_clique.push_back(v);
-            }
-        }
-        maximum_clique.push_back(G.deg_lists[G.max_degree].back());
+        maximum_clique= G.partial;
     }
 
     //for all v ∈ P
-    for(Vertex* branch_vertex = get_candidate(G); branch_vertex!=nullptr; branch_vertex = get_candidate(G)){
+    vector<Vertex*> candidates = get_candidates(G);
+    while(!candidates.empty()){
+        Vertex* branch_vertex = candidates.back();
+
         G.set_restore();
 
         //P' = P ∩ (N(v) ∪ {v})
-        MM_discard_strangers(G, branch_vertex);
+        G.MM_induced_neighborhood(branch_vertex);
 
         //C' = C ∪ {v} & P' = P'\{v}
         G.MM_clique_add_vertex(branch_vertex);
@@ -95,8 +56,9 @@ void branch_and_bound(Graph& G, vector<Vertex*>& maximum_clique){ //BnB(P, C, C^
         G.restore();
 
         //P = P\{v}
-        G.MM_discard_vertex(branch_vertex);
-
+        candidates.pop_back();
+        G.MM_induced_subgraph(candidates);
+ 
     }
 
     G.restore();
