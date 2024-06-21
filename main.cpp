6169a6bac43727c0475420751c663acd6988710b
eloinoel <eloi.sandt.games@gmx.de>
2024-06-21 15:22:20 +0200

    chore: prepare for merge to main

--- main.cpp
+++ main.cpp
@@ -24,32 +24,32 @@ int main(int argc, char**argv){
     Graph G;
     load_graph(G);
     //test_graph_consistency(G); //TODO: remove debug
-    std::cout << "Graph with N=" + std::to_string(G.N) + " and M=" + std::to_string(G.M) + " loaded." << std::endl;
+    //std::cout << "Graph with N=" + std::to_string(G.N) + " and M=" + std::to_string(G.M) + " loaded." << std::endl;
 
-    // #if DEBUG
-    //     G.timer.start("solve");
-    // #endif
+    #if DEBUG
+        G.timer.start("solve");
+    #endif
 
-    // //size_t vc_size = solve_k(G);
-    // //vector<Vertex*> maximum_clique = branch_and_bound_mc(G);
-    // //SolverViaVC solver = SolverViaVC();
-    // //int max_clique_size = solver.solve_via_vc(G);
+    //size_t vc_size = solve_k(G);
+    vector<Vertex*> maximum_clique = branch_and_bound_mc(G);
+    //SolverViaVC solver = SolverViaVC();
+    //int max_clique_size = solver.solve_via_vc(G);
 
     // #if !NDEBUG
     //     print_success("Found maximum clique of size " + std::to_string(max_clique_size));
     // #endif
 
-    // #ifdef RELEASE
-    //     //G.output_vc();
-    //     //print maximum clique
-    //     for(Vertex* v : maximum_clique){
-    //         cout << G.name_table[v->id] << "\n";
-    //     }
-    // #endif
+    #ifdef RELEASE
+        //G.output_vc();
+        //print maximum clique
+        for(Vertex* v : maximum_clique){
+            cout << G.name_table[v->id] << "\n";
+        }
+    #endif
 
-    // #if DEBUG
-    //     cout << "max clique has size " << max_clique_size << std::endl;
-    //     G.timer.report<chrono::milliseconds>("solve", true);
-    // #endif
+    #if DEBUG
+        cout << "max clique has size " << max_clique_size << std::endl;
+        G.timer.report<chrono::milliseconds>("solve", true);
+    #endif
     return 0;
 }
