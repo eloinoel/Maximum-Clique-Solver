28145836bbcf150e5c96495144e6fba685e93dc5
eloinoel <eloi.sandt.games@gmx.de>
2024-06-30 19:30:00 +0200

    refactor: cleaned up main

--- main.cpp
+++ main.cpp
@@ -17,9 +17,10 @@
 #include <thread>
 #include <future>
 
-#define BENCHMARK 0 //TODO: remove once benchmark.sh works
+#define BENCHMARK 0
 #define ACTIVE_SOLVER SOLVER::PARALLEL
 
+using namespace std;
 
 vector<string> launch_dOmega(Graph G){
     SolverViaVC solver;
@@ -42,39 +43,21 @@ int main(int argc, char**argv){
 
     Graph G;
     load_graph(G);
-    //test_graph_consistency(G); //TODO: remove debug
-    //std::cout << "Graph with N=" + std::to_string(G.N) + " and M=" + std::to_string(G.M) + " loaded." << std::endl;
-
-    #if DEBUG
-        G.timer.start("solve");
-    #endif
 
     SolverViaVC solver;
     vector<Vertex*> maximum_clique;
     int max_clique_size = -1;
-    vector<Vertex*> tmp_mc;
-
     future<vector<string>> results[2];
+
     switch(ACTIVE_SOLVER){
         case SOLVER::VIA_VC:
             solver = SolverViaVC();
             max_clique_size = solver.solve_via_vc(G);
-            //TODO: remove this debug code
-            // for(std::string v : solver.maximum_clique){
-            //     cout << v << endl;
-            // }
-            //print_success("Found maximum clique of size " + std::to_string(max_clique_size));
-            //tmp_mc = solver.convert_vertex_list(G, solver.maximum_clique);
-            //assert(solver.is_clique(tmp_mc));
             break;
         case SOLVER::BRANCH_AND_BOUND:
             maximum_clique = branch_and_bound_mc(G);
             max_clique_size = maximum_clique.size();
-            //TODO: remove this debug code
-            // for(Vertex* v : maximum_clique){
-            //     cout << G.name_table[v->id] << endl;
-            // }
-            // break;
+            break;
         case SOLVER::CLISAT:
             solve_clique(G);
             break;
@@ -82,18 +65,17 @@ int main(int argc, char**argv){
             {
                 bool copy_domega = true;
                 bool copy_cli = false; // ALL BUT LAST NEED TO BE COPIED
-            
                 if(copy_domega){
                     Graph H = G.shallow_copy();
-                    results[0] = std::async(std::launch::async, launch_dOmega, H);
+                    results[0] = async(launch::async, launch_dOmega, H);
                 }else{
-                    results[0] = std::async(std::launch::async, launch_dOmega, std::ref(G));
+                    results[0] = async(launch::async, launch_dOmega, ref(G));
                 }
                 if(copy_cli){
                     Graph H = G.shallow_copy();
-                    results[1] = std::async(std::launch::async, launch_cli, H);
+                    results[1] = async(launch::async, launch_cli, ref(H));
                 }else{
-                    results[1] = std::async(std::launch::async, launch_cli, std::ref(G));
+                    results[1] = async(launch::async, launch_cli, ref(G));
                 }
             }
             break;
@@ -101,20 +83,11 @@ int main(int argc, char**argv){
             print_warning("No solver selected");
             break;
     }
-    //size_t vc_size = solve_k(G);
-    //vector<Vertex*> maximum_clique = branch_and_bound_mc(G);
-
-
-    #if !NDEBUG
-        //print_success("Found maximum clique of size " + std::to_string(max_clique_size));
-    #endif
 
     #ifdef RELEASE
-        //G.output_vc();
-        //print maximum clique
         switch(ACTIVE_SOLVER){
             case SOLVER::VIA_VC:
-                for(std::string v : solver.maximum_clique){
+                for(string v : solver.maximum_clique){
                     cout << v << "\n";
                 }
                 break;
@@ -142,7 +115,7 @@ int main(int argc, char**argv){
                 auto clique_found = results[num].get();
                 for(string& s : clique_found)
                     cout << s << "\n";
-            }   
+            }
                 exit(EXIT_SUCCESS);
                 break;
             default:
@@ -151,9 +124,5 @@ int main(int argc, char**argv){
         }
     #endif
 
-    #if DEBUG
-        cout << "max clique has size " << max_clique_size << std::endl;
-        G.timer.report<chrono::milliseconds>("solve", true);
-    #endif
     return 0;
 }
